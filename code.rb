41f752a6d0c76f29e7eae4cc629c0d585fbd2113
maykazw <maykazw@gmail.com>
2020-03-25 15:25:24 +0100

    update code

--- code.rb
+++ code.rb
@@ -1,36 +1,80 @@
-elements = 4
-array_with_random_no = (0..9).to_a.sample(elements)
-guess = nil
-i=1
-print "\\|/          (__)
+class CowsAndBulls
+  attr_accessor :guess, :i,:elements,:array_with_random_no
+
+  def initialize
+    @elements = get_elements
+    @array_with_random_no = get_sample
+    @guess = nil
+    @i = 1
+    print "\\|/          (__)
      `\\------(oo)
        ||    (__)
        ||w--||     \\|/
    \\|/ \n\n Zgadnij liczbę: "
-loop do
-  bulls = 0
-  cows = 0
-  loop do
-    puts "Proba  #{i}: "
-    guess = gets.chomp.chars.map(&:to_i)
-    exit if guess.empty?
-    # Nie ma 4, nie ma unikalnych 4
-    break if guess.size == elements && guess.uniq.size == elements
-    puts "Wpisz #{elements} unikalne cyfry !"
   end
-  if guess == array_with_random_no
-    puts "Odgadłes za #{i} razem"
-    break
-  end
-  elements.times do |j|
-    if guess[j] == array_with_random_no[j]
-      bulls += 1
-      #   na tym samym miejscu w tablicy
-    elsif array_with_random_no.include? guess[j]
-      cows += 1
-    # zawiera w ciagu
+
+  def start_game
+    loop do
+      init_bulls_and_cows
+      loop do
+        puts "Proba  #{@i}: "
+        @guess = gets.chomp.chars.map(&:to_i)
+        exit if @guess.empty?
+        break if guess_check
+        increment_counter
+        puts "Wpisz #{@elements} unikalne cyfry !"
+      end
+      if @guess == @array_with_random_no
+        puts "Odgadłes za #{@i} razem"
+        break
+      end
+      @elements.times do |j|
+        if @guess[j] == @array_with_random_no[j]
+          increment_bulls
+        elsif @array_with_random_no.include? @guess[j]
+          increment_cows
+        end
+      end
+      increment_counter
+      return_score(@bulls,@cows)
     end
   end
-  i+=1
-  puts "Bykow: #{bulls} \n Krow: #{cows} \n"
+
+  private
+
+  def get_sample
+    (0..9).to_a.sample(@elements)
+  end
+
+  def get_elements
+    4
+  end
+
+  def guess_check
+    @guess.size == @elements && @guess.uniq.size == @elements
+  end
+
+  def init_bulls_and_cows
+    @bulls = 0
+    @cows = 0
+  end
+  
+  def return_score(bulls,cows)
+    puts "Bykow: #{bulls} \n Krow: #{cows} \n"
+  end
+
+  def increment_bulls
+    @bulls += 1
+  end
+
+  def increment_cows
+    @cows += 1
+  end
+
+  def increment_counter
+    @i += 1
+  end
 end
+
+game = CowsAndBulls.new
+game.start_game
